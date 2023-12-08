dbd62557da22cc69999c55f42b8d99c6096c4289
Bret Beheim <=>
2023-12-08 07:17:23 +0100

    added day 07 part 2 solution

--- day07/solution.R
+++ day07/solution.R
@@ -1,25 +1,35 @@
 
-typer <- function(x) {
-  # without joker rule
-  n_unique <- length(unique(x))
-  counts <- rev(sort(table(x)))
-  if (n_unique == 1) out <- "Five of a kind"
+typer <- function(x, wild = "") {
+  if (length(x) == 1 && nchar(x) == 5) x <- strsplit(x, split = "")[[1]]
+  n_wild <- sum(x == wild)
+  n_unique <- length(unique(x[x != wild]))
+  counts <- rev(sort(table(x[x != wild])))
+  if (n_unique %in% c(0, 1)) out <- "Five of a kind"
+  # AAAAA, AAAAJ, AAAJJ, AJJJJ, JJJJJ
   else if (n_unique == 2)
-    if (all(counts == c(4, 1))) out <- "Four of a kind"
-    else if (all(counts == c(3, 2))) out <- "Full house"
+    if (all(counts == c(4 - n_wild, 1))) out <- "Four of a kind" # AAAAB, AAABJ, AABJJ, ABJJJ
+    else if (all(counts == c(3 - n_wild, 2))) out <- "Full house"
+    # AAABB, AABBJ
     else stop("invalid hand with 2 unique cards")
   else if (n_unique == 3)
-    if (counts[1] == 3) out <- "Three of a kind"
-    else if (counts[1] == 2) out <- "Two pair"
+    if (counts[1] == 3 - n_wild) out <- "Three of a kind"
+    # AAABC, AABCJ, ABCJJ
+    else if (counts[1] == 2 - n_wild) out <- "Two pair"
+    # AABBC
     else stop("invalid hand with 3 unique cards")
   else if (n_unique == 4) out <- "One pair"
+  # AABCD, ABCDJ
   else if (n_unique == 5) out <- "High card"
+  # ABCDE
   else stop("unable to find type!")
-  # need new logic for joker rule...
   return(out)
 }
 
-calc_order_sum <- function(path) {
+calc_order_sum <- function(path, part1 = TRUE) {
+  if (part1) 
+    wild_card <- ""
+  else 
+    wild_card <- "J"
   x <- readLines(path)
   dat <- data.frame(
     bid = as.numeric(gsub("^\\w*\\s", "", x)),
@@ -30,7 +40,7 @@ calc_order_sum <- function(path) {
     handmat[i,] <- strsplit(dat$hand[i], split = "")[[1]]
   }
   dat$type <- rep(NA, length(x))
-  for (i in 1:length(x)) dat$type[i] <- typer(handmat[i,])
+  for (i in 1:length(x)) dat$type[i] <- typer(handmat[i,], wild = wild_card)
   types <- c("Five of a kind", "Four of a kind", "Full house", "Three of a kind", "Two pair", "One pair", "High card")
   dat$type <- factor(dat$type, levels = types)
   o <- order(dat$type, decreasing = TRUE)
@@ -39,7 +49,10 @@ calc_order_sum <- function(path) {
   handmat[which(handmat == "A")] <- "14"
   handmat[which(handmat == "K")] <- "13"
   handmat[which(handmat == "Q")] <- "12"
-  handmat[which(handmat == "J")] <- "11" # joker is worth 1 in part 2 instead of 10, lowest card rank
+  if (part1) 
+    handmat[which(handmat == "J")] <- "11"
+  else 
+    handmat[which(handmat == "J")] <- "1"
   handmat[which(handmat == "T")] <- "10"
   class(handmat) <- "integer"
   for (i in 1:length(types)) {
@@ -57,4 +70,9 @@ calc_order_sum <- function(path) {
 stopifnot(calc_order_sum("day07/test_input.txt") == 6440)
 tic("day 07, part 1")
 stopifnot(calc_order_sum("day07/input.txt") == 249204891)
+toc(log = TRUE)
+
+stopifnot(calc_order_sum("day07/test_input.txt", part1 == FALSE) == 5905)
+tic("day 07, part 2")
+stopifnot(calc_order_sum("day07/input.txt", part1 == FALSE) == 249666369)
 toc(log = TRUE)
